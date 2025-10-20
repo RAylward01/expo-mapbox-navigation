6330a7bfd77f015bdb4d1ec643bb144f090fd40d
RAylward01 <reinhard@codehesion.com>
2025-10-20 08:43:16 +0200

    Refactor event and property definitions for clarity

--- ios/ExpoMapboxNavigationModule.swift
+++ ios/ExpoMapboxNavigationModule.swift
@@ -1,84 +1,100 @@
 import ExpoModulesCore
 
 public class ExpoMapboxNavigationModule: Module {
-
   public func definition() -> ModuleDefinition {
     Name("ExpoMapboxNavigation")
-
+    
     View(ExpoMapboxNavigationView.self) {
-      Events("onRouteProgressChanged", "onCancelNavigation", "onWaypointArrival", "onFinalDestinationArrival", "onRouteChanged", "onUserOffRoute", "onRoutesLoaded", "onRouteFailedToLoad")
-
+      Events(
+        "onRouteProgressChanged",
+        "onCancelNavigation",
+        "onWaypointArrival",
+        "onFinalDestinationArrival",
+        "onRouteChanged",
+        "onUserOffRoute",
+        "onRoutesLoaded",
+        "onRouteFailedToLoad"
+      )
+      
       Prop("coordinates") { (view: ExpoMapboxNavigationView, coordinates: Array<Dictionary<String, Any>>) in
-         var points: Array<CLLocationCoordinate2D> = []
-         for coordinate in coordinates {
-            let longValue = coordinate["longitude"]
-            let latValue = coordinate["latitude"]
-            if let long = longValue as? Double, let lat = latValue as? Double {
-                points.append(CLLocationCoordinate2D(latitude: lat, longitude: long))
-            }
+        var points: Array<CLLocationCoordinate2D> = []
+        for coordinate in coordinates {
+          let longValue = coordinate["longitude"]
+          let latValue = coordinate["latitude"]
+          if let long = longValue as? Double, let lat = latValue as? Double {
+            points.append(CLLocationCoordinate2D(latitude: lat, longitude: long))
           }
-          view.controller.setCoordinates(coordinates: points) 
+        }
+        view.controller.setCoordinates(coordinates: points)
       }
-
+      
       Prop("vehicleMaxHeight") { (view: ExpoMapboxNavigationView, maxHeight: Double?) in
-          view.controller.setVehicleMaxHeight(maxHeight: maxHeight)
+        view.controller.setVehicleMaxHeight(maxHeight: maxHeight)
       }
-
+      
       Prop("vehicleMaxWidth") { (view: ExpoMapboxNavigationView, maxWidth: Double?) in
-          view.controller.setVehicleMaxWidth(maxWidth: maxWidth)
+        view.controller.setVehicleMaxWidth(maxWidth: maxWidth)
       }
-
+      
       Prop("locale") { (view: ExpoMapboxNavigationView, locale: String?) in
-          view.controller.setLocale(locale: locale) 
+        view.controller.setLocale(locale: locale)
       }
-
-      Prop("useRouteMatchingApi"){ (view: ExpoMapboxNavigationView, useRouteMatchingApi: Bool?) in
-          view.controller.setIsUsingRouteMatchingApi(useRouteMatchingApi: useRouteMatchingApi) 
+      
+      Prop("useRouteMatchingApi") { (view: ExpoMapboxNavigationView, useRouteMatchingApi: Bool?) in
+        view.controller.setIsUsingRouteMatchingApi(useRouteMatchingApi: useRouteMatchingApi)
       }
-
-      Prop("waypointIndices"){ (view: ExpoMapboxNavigationView, indices: Array<Int>?) in
-          view.controller.setWaypointIndices(waypointIndices: indices) 
+      
+      Prop("waypointIndices") { (view: ExpoMapboxNavigationView, indices: Array<Int>?) in
+        view.controller.setWaypointIndices(waypointIndices: indices)
       }
-
-      Prop("routeProfile"){ (view: ExpoMapboxNavigationView, profile: String?) in
-          view.controller.setRouteProfile(profile: profile) 
+      
+      Prop("routeProfile") { (view: ExpoMapboxNavigationView, profile: String?) in
+        view.controller.setRouteProfile(profile: profile)
       }
-
-      Prop("routeExcludeList"){ (view: ExpoMapboxNavigationView, excludeList: Array<String>?) in
-          view.controller.setRouteExcludeList(excludeList: excludeList) 
+      
+      Prop("routeExcludeList") { (view: ExpoMapboxNavigationView, excludeList: Array<String>?) in
+        view.controller.setRouteExcludeList(excludeList: excludeList)
       }
-
-      Prop("mapStyle"){ (view: ExpoMapboxNavigationView, style: String?) in
-          view.controller.setMapStyle(style: style) 
+      
+      Prop("mapStyle") { (view: ExpoMapboxNavigationView, style: String?) in
+        view.controller.setMapStyle(style: style)
       }
-
-      Prop("mute"){ (view: ExpoMapboxNavigationView, isMuted: Bool?) in
-          view.controller.setIsMuted(isMuted: isMuted) 
+      
+      Prop("mute") { (view: ExpoMapboxNavigationView, isMuted: Bool?) in
+        view.controller.setIsMuted(isMuted: isMuted)
       }
-
+      
       Prop("initialLocation") { (view: ExpoMapboxNavigationView, location: Dictionary<String, Any>?) in
         if(location != nil){
           let longValue = location!["longitude"]
           let latValue = location!["latitude"]
           let zoomValue = location!["zoom"]
           if let long = longValue as? Double, let lat = latValue as? Double, let zoom = zoomValue as? Double? {
-              view.controller.setInitialLocation(location: CLLocationCoordinate2D(latitude: lat, longitude: long), zoom: zoom)
+            view.controller.setInitialLocation(location: CLLocationCoordinate2D(latitude: lat, longitude: long), zoom: zoom)
           }
         }
       }
-
+      
       Prop("customRasterSourceUrl") { (view: ExpoMapboxNavigationView, url: String?) in
         view.controller.setCustomRasterSourceUrl(url: url)
       }
-
+      
       Prop("placeCustomRasterLayerAbove") { (view: ExpoMapboxNavigationView, layerId: String?) in
         view.controller.setPlaceCustomRasterLayerAbove(layerId: layerId)
       }
-
+      
       Prop("disableAlternativeRoutes") { (view: ExpoMapboxNavigationView, disableAlternativeRoutes: Bool?) in
         view.controller.setDisableAlternativeRoutes(disableAlternativeRoutes: disableAlternativeRoutes)
       }
-
+      
+      Prop("force2D") { (view: ExpoMapboxNavigationView, force2D: Bool?) in
+        view.controller.setForce2D(force2D: force2D)
+      }
+      
+      Prop("useMetricUnits") { (view: ExpoMapboxNavigationView, useMetric: Bool?) in
+        view.controller.setUseMetricUnits(useMetric: useMetric)
+      }
+      
       AsyncFunction("recenterMap") { (view: ExpoMapboxNavigationView) in
         view.controller.recenterMap()
       }
