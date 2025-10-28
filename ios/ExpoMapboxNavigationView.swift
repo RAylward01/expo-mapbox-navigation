17a585477db8709e1d99eb0cb45789a65943b1cd
RAylward01 <reinhard@codehesion.com>
2025-10-28 20:18:30 +0200

    Update ExpoMapboxNavigationView.swift

--- ios/ExpoMapboxNavigationView.swift
+++ ios/ExpoMapboxNavigationView.swift
@@ -487,7 +487,7 @@ class ExpoMapboxNavigationViewController: UIViewController {
         navigationViewController.showsContinuousAlternatives = false
         navigationViewController.usesNightStyleWhileInTunnel = false
         navigationViewController.automaticallyAdjustsStyleForTimeOfDay = false
-        navigationViewController?.navigationMapView.mapView.preferredFramesPerSecond = 20
+        navigationViewController.navigationMapView.mapView.preferredFramesPerSecond = 20
 
         let navigationMapView = navigationViewController.navigationMapView
 
